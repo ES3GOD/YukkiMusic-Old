6d7f308561862e03e932d33c465af20905245158
~ES3GOD <89799912+ES3GOD@users.noreply.github.com>
2022-05-23 13:33:22 +0530

    Update paste.py

--- Yukki/YukkiUtilities/helpers/paste.py
+++ Yukki/YukkiUtilities/helpers/paste.py
@@ -1,24 +1,21 @@
-import socket
-import asyncio
-from functools import partial
+import aiohttp
+
+BASE = "https://batbin.me/"
 
 
-def _netcat(host, port, content):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
-    s.sendall(content.encode())
-    s.shutdown(socket.SHUT_WR)
-    while True:
-        data = s.recv(4096).decode("utf-8").strip("\n\x00")
-        if not data:
-            break
+async def post(url: str, *args, **kwargs):
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, *args, **kwargs) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
         return data
-    s.close()
 
 
-async def paste(content):
-    loop = asyncio.get_running_loop()
-    link = await loop.run_in_executor(
-        None, partial(_netcat, "ezup.dev", 9999, content)
-    )
+async def Yukkibin(text):
+    resp = await post(f"{BASE}api/v2/paste", data=text)
+    if not resp["success"]:
+        return
+    link = BASE + resp["message"]
     return link
